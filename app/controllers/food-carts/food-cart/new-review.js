0946aa89a4d7c24a9ed1c68399265aeb66250a7f
David Garber & Jeff Lalic <student@epicodus.com>
2015-06-18 14:47:24 -0700

    Allow user to assign a rating to their review.

--- app/controllers/food-carts/food-cart/new-review.js
+++ app/controllers/food-carts/food-cart/new-review.js
@@ -13,11 +13,19 @@ export default Ember.Controller.extend({
   //     });
   //   }
   needs: ['food-carts/food-cart'],
+  selectContentType: [
+    {label: "5", value: 5},
+    {label: "4", value: 4},
+    {label: "3", value: 3},
+    {label: "2", value: 2},
+    {label: "1", value: 1}
+  ],
   actions: {
     submit: function() {
       var newReview = this.store.createRecord('review', {
         user: this.get('user'),
-        text: this.get('text')
+        text: this.get('text'),
+        rating: this.get('selectedContentType.value')
       });
       var cart = this.get('controllers.food-carts/food-cart.model');
       newReview.save().then(function() {
--- app/models/review.js
+++ app/models/review.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   user: DS.attr('string'),
   text: DS.attr('string'),
-  foodCart: DS.belongsTo("food-cart", {async: true})
+  foodCart: DS.belongsTo("food-cart", {async: true}),
+  rating: DS.attr('number')
 });
